6c1e5631c02ca5dca93fb9c680f2ab01165c3794
adovg <you@example.com>
2020-07-09 23:28:13 +0300

    work section done

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,1 @@
-function test(){alert("hello js");let e=firstNum+secondNum;console.console.log(e)}let firstNum=5,secondNum=10;test();
+$(document).ready(function(){console.log("document ready"),$(".works__btn button, .works__btn a").on("focus",function(){$(this).parents(".works__element").addClass("works__element--active")}),$(".works__btn button, .works__btn a").on("blur",function(e){$(this).parents(".works__element").removeClass("works__element--active")})});
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,26 @@
-function test() {
-	alert('hello js');
-	let sum = (firstNum + secondNum);
-	console.console.log(sum);
-};
+// function test() {
+// 	alert('hello js');
+// 	let sum = (firstNum + secondNum);
+// 	console.log(sum);
+// };
 
 
 
-let firstNum = 5,
-	secondNum = 10;
+// let firstNum = 5,
+// 	secondNum = 10;
 
-test();
+// test();
+
+$(document).ready(function () {
+	console.log('document ready');
+	
+	$('.works__btn button, .works__btn a').on('focus', function() {
+		// alert('!')
+		 $(this).parents('.works__element').addClass('works__element--active');
+	});
+
+	$('.works__btn button, .works__btn a').on('blur', function(e) {
+		$(this).parents('.works__element').removeClass('works__element--active');
+	});
+
+});
