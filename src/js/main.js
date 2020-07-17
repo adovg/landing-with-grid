f45247d95c3d4527bf42b49b096b2984fc818671
adovg <you@example.com>
2020-07-17 22:47:41 +0300

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,53 @@ $(document).ready(function () {
 		$(this).parents('.works__element').removeClass('works__element--active');
 	});
 
+	function moveProgressBar(node, nodeLine, tooltip, animationLength = 600) {
+		const progressElement = $(node);
+		progressElement.each(function (value, item) {
+			$(item).find(nodeLine).animate({
+				width: item.dataset.progressPercent+'%'
+			}, animationLength);
+			$(item).find(tooltip).show(animationLength);
+		})
+	};
+
+	
+	let animate = true;
+
+$(window).scroll(function () {
+
+	if ($('.skills').offset().top <= $(window).scrollTop() + 200 ) {
+		if (animate) {
+		moveProgressBar('.progress__element', '.progress__line', '.progess__tooltip');
+		 }
+		 animate = false;
+ }
 });
+
+// jQuery(function($){ ...your code... });
+
+$('.carousel').owlCarousel({
+    loop:true,
+    margin:0,
+	nav:true,
+	dots:false,
+	navText: [],
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+		},
+		980:{
+            items:5
+        }
+    }
+})
+
+
+});
+
